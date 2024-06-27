4eb4e66029de4d6a0ef1ca1cda2e87ac4d02abca
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-06-27 04:24:34 +0300

    update readme

--- compile_all.py
+++ compile_all.py
@@ -1,9 +1,15 @@
 import os
+import sys
 
-folder = "specsec"
+if len(sys.argv) <= 1:
+    print('Enter source folder name as command argument')
+    exit(1)
+
+folder = sys.argv[1]
+BLACKLIST = ['exam']
 
 for i in os.listdir(folder):
-    if i.endswith('tex') and 'exam' not in i:
+    if i.endswith('tex') and all([j not in i for j in BLACKLIST]):
         os.system("python linter.py " + os.path.join(".", folder, i))
 
 os.system(f'python superconspect.py {folder}')
