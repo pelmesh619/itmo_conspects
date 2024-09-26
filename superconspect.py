7f08583801c724f7aae0e0b89273659630fb3519
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-09-26 23:13:39 +0300

    feat superconspect generates toc in md

--- superconspect.py
+++ superconspect.py
@@ -2,6 +2,8 @@ import os
 import re
 import sys
 
+from urllib.parse import quote
+
 if len(sys.argv) <= 1:
     print('Enter source folder name as command argument')
     exit(1)
@@ -13,11 +15,37 @@ if all(map(lambda x: x.endswith('.md') or '.' not in x, os.listdir(folder))):
     supername = folder + "_superconspect.md"
     BLACKLIST = [supername]
 
+    contents = {}
+
     text = ''
 
     for i in os.listdir(folder):
-        if i.endswith('.md') and not any([j in i for j in BLACKLIST]):
-            text += open(os.path.join(folder, i), 'r', encoding='utf8').read()
+        if not i.endswith('.md') or any([j in i for j in BLACKLIST]):
+            continue
+
+        file_text = open(os.path.join(folder, i), 'r', encoding='utf8').read()
+        for header_match in re.finditer('(^|\n)(#+) +(.+)\n', file_text, re.U):
+            header_level = len(header_match.group(2))
+            header_name = header_match.group(3).strip()
+
+            header_link = quote(header_name.lower().replace(' ', '-'), safe='', encoding='utf8')
+            contents['#' + header_link] = (header_name, header_level)
+
+        text += file_text + '\n\n'
+
+    table_of_contents = ''
+    for link, (header, header_level) in contents.items():
+        table_of_contents += '  ' * (header_level - 1) + '* ' + f'[{header}]({link})\n'
+
+    first_header = re.search('#+.+\n', text)
+    first_header_text = None
+    if first_header:
+        first_header_text = first_header.group(0)
+        text = text[len(first_header_text):]
+
+    text = table_of_contents + '\n\n' + text
+    if first_header:
+        text = first_header_text + '\n\n' + text
 
     open(os.path.join(folder, supername), 'w', encoding='utf8').write(text)
 
