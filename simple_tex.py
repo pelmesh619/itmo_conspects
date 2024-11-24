744eb59f017a6060d94e2a35d51796604e030f93
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-11-24 21:43:48 +0300

    refactor a bit in simple_tex.py

--- simple_tex.py
+++ simple_tex.py
@@ -23,32 +23,43 @@ template = r"""
 \end{document}
 """
 
-if len(sys.argv) <= 1:
-    print('Enter source filename as command argument')
-    exit(1)
+def make_full_doc(content):
+    global template
 
-show_warnings = False
-if '--warning' in sys.argv or '-w' in sys.argv:
-    show_warnings = True
- 
-filename = sys.argv[1]
-folder = os.path.join(*os.path.split(filename)[:-1])
+    t = template
+    new_content = content
+    for i in re.finditer(r'((\n)|(^))(\$\w+\$)\=(.*)', content):
+        if i.group(4) in t:
+            t = t.replace(i.group(4), i.group(5), 1)
+            new_content = new_content.replace(i.group(0), '\n', 1)
 
-file = open(filename, encoding='utf8').read() + '\n'
+    new_content = re.sub('(.+)\n', r'    \g<1>\n', new_content + '\n')
 
-file = re.sub('(.+)\n', r'    \g<1>\n', file)
+    new_content = t.replace('% content %', new_content, 1)
 
-for i in re.finditer(r'((\n)|(^))\$(.+)\$\=(.+)\n', file):
-    template = template.replace(i.group(4), i.group(5), 1)
+    return new_content
 
-new_text = template.replace('% content %', file, 1)
 
-if not os.path.exists('linted'):
-    os.mkdir('linted')
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print('Enter source filename as command argument')
+        exit(1)
 
-new_filename = os.path.join('linted', os.path.split(filename)[-1])
-open(new_filename, 'w', encoding='utf8').write(new_text)
+    show_warnings = False
+    if '--warning' in sys.argv or '-w' in sys.argv:
+        show_warnings = True
+    
+    filename = sys.argv[1]
+    folder = os.path.join(*os.path.split(filename)[:-1])
 
-os.system(f"python linter.py {new_filename} "
-          f"{'-w' if show_warnings else ''} "
-          f"-o {os.path.join('conspects', folder)}")
+    new_text = make_full_doc(open(filename, encoding='utf8').read())
+
+    if not os.path.exists('linted'):
+        os.mkdir('linted')
+
+    new_filename = os.path.join('linted', os.path.split(filename)[-1])
+    open(new_filename, 'w', encoding='utf8').write(new_text)
+
+    os.system(f"python linter.py {new_filename} "
+            f"{'-w' if show_warnings else ''} "
+            f"-o {os.path.join('conspects', folder)}")
