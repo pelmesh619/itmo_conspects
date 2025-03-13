8040eb69deb55b02d77e46bd229ac2bfc5c7bad4
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-03-13 20:19:44 +0300

    add support for simple_tex

--- compile_all.py
+++ compile_all.py
@@ -10,6 +10,11 @@ BLACKLIST = ['exam']
 
 for i in os.listdir(folder):
     if i.endswith('tex') and all([j not in i for j in BLACKLIST]):
-        os.system("python linter.py " + os.path.join(".", folder, i))
+        text = open(os.path.join(".", folder, i), encoding='utf-8').read()
+        if '\\begin{document}' in text:
+            os.system("python linter.py " + os.path.join(".", folder, i))
+        else:
+            os.system("python simple_tex.py " + os.path.join(".", folder, i))
+
 
 os.system(f'python superconspect.py {folder}')
