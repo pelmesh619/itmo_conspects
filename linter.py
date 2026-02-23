41b0e23dfdcf2388f5f8c1c7572187ea8517db8a
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-02-23 17:13:08 +0300

    refactor tex build script

--- linter.py
+++ linter.py
@@ -1,86 +1,31 @@
+import argparse
 import re
 import time
 import shutil
 import sys, os
 
+from pathlib import Path
+
+from assets.build.tex.conspect_builder import TexConspectBuilder
+
 # for making tex *** fancier ***
 
-DEFAULT_OUTPUT_DIRECTORY = "./conspects/"
+DEFAULT_OUTPUT_DIRECTORY = Path("conspects")
 
-if len(sys.argv) <= 1:
-    print('Enter source filename as command argument')
-    exit(1)
+parser = argparse.ArgumentParser(
+    prog='linter.py',
+    description='This program helps you to build source files into ready-to-read PDFs!'
+)
+parser.add_argument('input_filename', help="Path to an input TeX file")
+parser.add_argument('-o', '--output-directory', help="Path to a directory of the file to be compiled")
+parser.add_argument('-l', '--linted-output', help="Path to a directory which will store temporary files", default='linted')
+parser.add_argument('-v', '--verbose', action='store_true', help="Show verbose output")
+parser.add_argument('-f', '--force', action='store_true', help="Force continued processing past errors")
+parser.add_argument('--bibtex', action='store_true', help="Uses bibtex (by default bibtex isn't used)")
 
-show_warnings = False
-if '--warning' in sys.argv or '-w' in sys.argv:
-    show_warnings = True
+args = parser.parse_args()
 
-linted_output = None
-if '--linted-output' in sys.argv or '-l' in sys.argv:
-    ind = sys.argv.index('-l') if '-l' in sys.argv else sys.argv.index('--linted-output')
+t = TexConspectBuilder(args.input_filename, None, args.linted_output)
 
-    if ind + 1 < len(sys.argv):
-        linted_output = sys.argv[ind + 1]
-
-output_directory = None
-if '--output-directory' in sys.argv or '-o' in sys.argv:
-    ind = sys.argv.index('-o') if '-o' in sys.argv else sys.argv.index('--output-directory')
-
-    if ind + 1 < len(sys.argv):
-        output_directory = sys.argv[ind + 1]
- 
-filename = sys.argv[1]
-folder = os.path.join(*os.path.split(filename)[:-1])
-
-file_text = open(filename, encoding='utf8').read()
-file = "%% THIS FILE IS GENERATED AUTOMATICALLY BY linter.py, ALL CHANGES WILL BE LOST\n\n\n"
-
-if os.path.exists(os.path.join(folder, '__preamble.sty')):
-    specific_preamble = open(os.path.join(folder, '__preamble.sty'), encoding='utf8').read()
-    if re.search(r'\\begin\{document\}', file_text):
-        file_text = file_text.replace('\\begin{document}', specific_preamble + '\n' + '\\begin{document}', 1)
-    else:
-        file += specific_preamble
-
-file += file_text
-
-
-
-to_display = True
-
-for i in list(re.finditer(r'(\$.+?\$)|(%nodisplay)|(%yesdisplay)', file, re.MULTILINE | re.DOTALL)):
-    if '%nodisplay' in i.group(0):
-        to_display = False
-    if '%yesdisplay' in i.group(0):
-        to_display = True
-
-    if not to_display:
-        continue
-
-    if r'\displaystyle' in i.group(0):
-        continue
-    if any(map(lambda x: x in i.group(0), [r'\frac', r'\sum', r'\int', r'\iint', r'\iiint', '^', '_', r'\lim'])):
-        file = file.replace(i.group(0), r'$\displaystyle ' + i.group(0)[1:])
-
-
-if not os.path.exists('linted'):
-    os.mkdir('linted')
-
-if linted_output is None:
-    linted_output = os.path.join('linted', os.path.split(filename)[-1])
-open(linted_output, 'w', encoding='utf8').write(file)
-
-interaction = 'nonstopmode' if show_warnings else 'batchmode'
-
-print(f'\n\nRendering {filename}...\n')
-
-start_time = time.time()
-
-os.system(f"pdflatex -file-line-error -interaction={interaction} -synctex=1 -output-format=pdf "
-          f"-output-directory=\"{os.path.join(DEFAULT_OUTPUT_DIRECTORY, folder) if output_directory is None else output_directory}\" "
-          f"-aux-directory=./auxil/ "
-          f"{linted_output} "
-          f"-file-line-error")
-
-print(f'\nRender of {filename} completed in {round(time.time() - start_time, 2)} s!')
+exit(t.build(args))
 
