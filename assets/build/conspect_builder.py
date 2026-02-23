9f1595e85610e46f42cc3929dcd2c83a7abc72ab
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-02-24 00:59:34 +0300

    extend abstract classes

--- assets/build/conspect_builder.py
+++ assets/build/conspect_builder.py
@@ -2,3 +2,6 @@ class ConspectBuilder:
     def __init__(self, input_filename, output_filename=None):
         self.input_filename = input_filename
         self.output_filename = output_filename
+
+    def build(args):
+        raise NotImplementedError()
--- assets/build/superconspect_builder.py
+++ assets/build/superconspect_builder.py
@@ -1,6 +1,17 @@
+from pathlib import Path
+
 class SuperconspectBuilder:
     def __init__(self, input_folder, blacklist_words=[], output_filename=None):
         self.input_folder = input_folder
         self.output_filename = output_filename
         self.blacklist_words = blacklist_words
 
+    @property
+    def superconspect_filename(self):
+        if self.output_filename is None:
+            return Path(self.input_folder) / (Path(self.input_folder).name + "_superconspect" + self.FILE_EXTENSION)
+
+        return self.output_filename
+
+    def build(args):
+        raise NotImplementedError()
