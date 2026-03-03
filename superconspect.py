7c77fb544062b51b511d57f51c6918bb530b4b2e
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-03-03 03:09:02 +0300

    refactor superconspect.py

--- superconspect.py
+++ superconspect.py
@@ -8,6 +8,10 @@ import sys
 from pathlib import Path
 from urllib.parse import quote
 
+from assets.build.markdown.superconspect_builder import MarkdownSuperconspectBuilder
+from assets.build.typst.superconspect_builder import TypstSuperconspectBuilder
+from assets.build.tex.superconspect_builder import TexSuperconspectBuilder
+
 BLACKLIST_WORDS = [
     'cheatsheet', 
     "superconspect", 
@@ -18,33 +22,44 @@ parser = argparse.ArgumentParser(
     description='This program helps you to build superconspect from multiple tex/md/typ-files to one big file!'
 )
 parser.add_argument('input_directory', help="Path to an input directory")
-parser.add_argument('--verbose', action='store_true', help="Show verbose output")
+parser.add_argument(
+    '-o', '--output-filename', 
+    help="Path to a ready file (.pdf or .md) to be compiled "
+    "(by default is `conspects/subjectname/subjectname_superconspect.pdf` or `subjectname/subjectname_superconspect.md`)"
+)
+parser.add_argument('-s', '--source-filename', help="Path to a source superconspect file to be generated (by default is `subjectname/subjectname_superconspect.tex/typ`) (for Tex and Typst only)")
+parser.add_argument('--watch', action='store_true', help="Watches an input file and recompiles on changes (for Typst only)")
+parser.add_argument('-l', '--linted-output', help="Path to a directory which will store temporary files (for Tex only)", default='linted')
+parser.add_argument('-v', '--verbose', action='store_true', help="Show verbose output (for Tex only)")
+parser.add_argument('-f', '--force', action='store_true', help="Force continued processing past errors (for Tex only)")
+parser.add_argument('--bibtex', action='store_true', help="Uses bibtex (by default bibtex isn't used) (for Tex only)")
+parser.add_argument('--only-linter', action='store_true', help="Only uses linter, do not compiles the actual PDF (for Tex only)")
 
 args = parser.parse_args()
 
-folder_files = [i for i in os.listdir(args.input_directory) if not i.startswith('__')]
+folder_files = [
+    i for i in os.listdir(args.input_directory) 
+    if not i.startswith('__') and
+        not i.endswith('.log') and
+        not i.endswith('.aux') and
+        not i.endswith('.fls') and
+        not i.endswith('.fdb_latexmk')
+]
 folder_files.sort()
 
-if all(map(lambda x: x.endswith('.md') or '.' not in x, folder_files)):
-    from assets.build.markdown.superconspect_builder import MarkdownSuperconspectBuilder
+if list(map(lambda x: x.endswith('.md'), folder_files)).count(True) >= len(folder_files) / 2:
+    t = MarkdownSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS, args.output_filename)
 
-    t = MarkdownSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS)
+elif list(map(lambda x: x.endswith('.typ'), folder_files)).count(True) >= len(folder_files) / 2:
+    t = TypstSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS, args.output_filename, args.source_filename)
 
-    t.build(args)
+elif list(map(lambda x: x.endswith('.tex'), folder_files)).count(True) >= len(folder_files) / 2:
+    t = TexSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS, args.output_filename, args.source_filename)
 
-    print("Markdown file is combined!")
-
-elif all(map(lambda x: x.endswith('.typ') or '.' not in x, folder_files)):
-    from assets.build.typst.superconspect_builder import TypstSuperconspectBuilder
-
-    t = TypstSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS)
-
-    t.build(args)
 else:
-    from assets.build.tex.superconspect_builder import TexSuperconspectBuilder
+    print(f'In folder `{args.input_directory}` there are less `.md`, `.typ`, `.tex` than others, I cannot detect file extension')
+    exit(1)
 
-    t = TexSuperconspectBuilder(args.input_directory, BLACKLIST_WORDS)
-
-    t.build(args)
+exit(t.build(args))
 
 
